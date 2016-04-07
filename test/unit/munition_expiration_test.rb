c8188b89d80f3e03fb76b43f6983afb6e9ee63a4
Clay Sampson <pdgeek@gmail.com>
2016-04-07 08:11:22 -0400

    munition expiration test

--- test/unit/munition_expiration_test.rb
+++ test/unit/munition_expiration_test.rb
@@ -5,7 +5,7 @@ require_relative '../../config/config'
 require_relative '../../lib/game_engine/all'
 require_relative '../helpers/fleet_manager_test_helper'
 
-class CollisionTest < MiniTest::Test
+class MunitionExpirationTest < MiniTest::Test
   def setup
     # need some static and some moving mobs
     @gs = Paidgeeks::RubyFC::Engine::GameState.new($stdout)
@@ -31,36 +31,13 @@ class CollisionTest < MiniTest::Test
       "energy" => Paidgeeks::RubyFC::Templates::Gunship.max_energy,
       "hitpoints" => Paidgeeks::RubyFC::Templates::Gunship.hit_points,
       "last_scan_tick" => 0,
+      "target_mid" => nil,
       "fleet_source" => false,
       })
     ndx += 1
-
-    # 2
-    @gs.add_fleet(ndx, FleetManagerTestHelper.new(ndx), 0, $stdout)
-    @gs.mobs[ndx] = Paidgeeks::RubyFC::Mob.from_msg({ 
-      "x_pos" => 1030, # about 1.5 ticks away for a rocket
-      "y_pos" => 1000,
-      "heading" => 0.0,
-      "velocity" => 0.0,
-      "mid" => @gs.reserve_mid,
-      "fid" => ndx,
-      "template" => Paidgeeks::RubyFC::Templates::Fighter,
-      "create_time" => @gs.time,
-      "turn_rate" => 0.0,
-      "valid_time" => @gs.time,
-      "turn_start_time" => 0.0,
-      "turn_stop_time" => 0.0,
-      "turn_stop" => 0,
-      "energy" => Paidgeeks::RubyFC::Templates::Fighter.max_energy,
-      "hitpoints" => Paidgeeks::RubyFC::Templates::Fighter.hit_points,
-      "last_scan_tick" => 0,
-      "fleet_source" => false,
-      })
-    ndx += 1
-
   end
 
-  def test_collisions
+  def test_munition_expiration
     ke = Paidgeeks::RubyFC::Engine::KinematicEngine.new(@gs)
     smp = Paidgeeks::RubyFC::Engine::SanitizedMessageProcessor.new
     gsc = Paidgeeks::RubyFC::Engine::GameStateChanger
@@ -68,20 +45,42 @@ class CollisionTest < MiniTest::Test
     smp.fire_msg({
       "type" => "fire",
       "munition_type" => "Rocket",
-      "munition_heading" => 90.0,
+      "munition_heading" => 0.0,
       "source_ship" => 1,
+      "target_mid" => 0,
       }, fm, @gs)
 
-    last_time = @gs.time
-    gsc::tick_msg(@gs, {"type" => "tick", "fleet_source" => false})
-    ke.update(last_time, @gs)
-
+    assert(@gs.mobs.size() == 2, "Rocket successfully fired")
 
     last_time = @gs.time
-    gsc::tick_msg(@gs, {"type" => "tick", "fleet_source" => false})
-    ke.update(last_time, @gs)
+    expire_time = last_time + @gs.config[:rocket_life_time]
+    while @gs.time < expire_time + 0.010
+      gsc::tick_msg(@gs, {"type" => "tick", "fleet_source" => false})
+      ke.update(last_time, @gs)
+      last_time = @gs.time
+    end
 
-    assert(@gs.mobs.size() == 1, "Rocket intercept kills both rocket and target")
+    assert(@gs.mobs.size() == 1, "Rocket expires when its supposed to")
+
+    smp.fire_msg({
+      "type" => "fire",
+      "munition_type" => "Missile",
+      "munition_heading" => 0.0,
+      "source_ship" => 1,
+      "target_mid" => 0,
+      }, fm, @gs)
+
+    assert(@gs.mobs.size() == 2, "Missile successfully fired")
+
+    last_time = @gs.time
+    expire_time = last_time + @gs.config[:missile_life_time]
+    while @gs.time < expire_time + 0.010
+      gsc::tick_msg(@gs, {"type" => "tick", "fleet_source" => false})
+      ke.update(last_time, @gs)
+      last_time = @gs.time
+    end
+
+    assert(@gs.mobs.size() == 1, "Missile expires when its supposed to")
 
   end
 end
