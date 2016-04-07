d3c334e4e5de486aeff4f42714cd252d4a99a2b8
Clay Sampson <pdgeek@gmail.com>
2016-04-07 06:12:17 -0400

    working towards final execution strategy

--- services/rubyfc_main.rb
+++ services/rubyfc_main.rb
@@ -1,12 +1,6 @@
 #!/usr/bin/env ruby
 require 'active_record'
-require 'optparse'
 require 'yaml'
-require_relative '../lib/game_engine/game_coordinator'
-require_relative '../lib/game_engine/transcript'
-require_relative '../config/constants'
-require_relative '../config/config'
-require_relative '../lib/managers/all'
 
 SIGNAL_QUEUE = []
 Signal.trap "TERM" do
@@ -20,57 +14,9 @@ db = YAML::load_file(Paidgeeks::RubyFC::DB_YML_PATH)
 env = ENV.has_key?("RAILS_ENV") ? ENV["RAILS_ENV"] : "production"
 ActiveRecord::Base.establish_connection(db[env])
 
-def parse_command_line
-  result = {
-    fleets: [],
-    game_log_file_name: "game.log",
-    just_show_transcript: false,
-    game_id: 1,
-    mission: "Paidgeeks::RubyFC::Missions::Deathmatch",
-  }
-  OptionParser.new do |opts|
-    opts.on("--fleet=", "Specify fleet file") do |opt|
-      result[:fleets] << opt
-    end
-    opts.on("--log_file=", "Specify the game log file") do |opt|
-      result[:game_log_file_name] = opt
-    end
-
-    opts.on("--just-show-transcript", "Just show the transcript from the game log, do not actually play it back") do
-      result[:just_show_transcript]=true
-    end
-
-    opts.on("--mission=", "Set mission name") do |opt|
-      result[:mission] = opt
-    end
-  end.parse!
-  
-  result
-end
-
 def main
-  opts = parse_command_line
-  if not opts[:just_show_transcript]
-    gc = Paidgeeks::RubyFC::Engine::GameCoordinator.new
-
-    File.open(File.join(Paidgeeks::RubyFC::LOG_DIR, opts[:game_log_file_name]),"w+t") do |journal|
-      opts[:journal] = journal
-
-      gc.game_setup(opts)
-
-      last_time = gc.gs.time
-      while !SIGNAL_QUEUE.any? and :in_progress == gc.game_tick(last_time)
-        last_time = gc.gs.time
-        Thread.pass
-      end
-      report = gc.gs.mission.mission_report(gc.gs)
-
-      puts("Mission report: #{report.inspect}")
-
-      gc.cleanup
-    end # File.open journal
-  else
-    Paidgeeks::RubyFC::Engine::Transcript::playback_until(opts) { SIGNAL_QUEUE.any? }
+  while not SIGNAL_QUEUE.any?
+    sleep 1.0
   end
 end
 
