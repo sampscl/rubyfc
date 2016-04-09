5218f25c734dee313f77ba21ca07e5ebbff65258
Clay Sampson <pdgeek@gmail.com>
2016-04-09 10:05:43 -0400

    garbage

--- garbage.rb
+++ garbage.rb
@@ -1,1 +1,15 @@
 #! /usr/bin/env ruby
+
+class Foo
+  attr_accessor :bar
+  def initialize
+    @bar = 1
+  end
+  def test(val)
+    self.bar=val
+  end
+end
+
+foo = Foo.new
+foo.test(27)
+puts foo.bar
