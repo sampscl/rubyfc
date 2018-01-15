0d29497616e2a4830566a48ca5dfc3c148a0f578
Clay <pdgeek@gmail.com>
2018-01-15 09:49:32 -0500

    beginnings of a message processing loop

--- lib/ui/tcl/ui_main.rb
+++ lib/ui/tcl/ui_main.rb
@@ -2,10 +2,23 @@ require 'rubygems'
 require 'bundler/setup'
 require 'tk'
 
+require_relative '../../game_engine/all'
+require_relative '../../utilities/stream_comms'
+
 module Paidgeeks
   module RubyFC
     module UI
       class TkMain
+
+        attr_accessor :all_msgs
+        attr_accessor :fleets
+        attr_accessor :mobs
+
+        def initialize
+          self.all_msgs = []
+          self.fleets = []
+          self.mobs = []
+        end
 
         # Get log file and log file name as [log_file, log_file_name]. Call
         # log_file.close() when done with it.
@@ -22,14 +35,14 @@ module Paidgeeks
         end
 
         def run
-          log_file, log_file_name = open_log_file
-          return 0 if log_file.nil?
+          @log_file, log_file_name = open_log_file
+          return 0 if @log_file.nil?
           begin
-            @root = TkRoot.new { title "Fleet Commander Playback - #{log_file_name}" }
+            @root = TkRoot.new { title "Fleet Commander Playback: #{log_file_name}" }
             @lists_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
-            @fleets = TkListbox.new(@lists_frame) { height 4 }
-            @mobs = TkListbox.new(@lists_frame) { height 25 }
-            @log = TkListbox.new(@lists_frame) { height 10 }
+            @fleets = TkListbox.new(@lists_frame) { listvariable @fleets ; height 4 }
+            @mobs = TkListbox.new(@lists_frame) { listvariable @mobs ; height 25 }
+            @log = TkListbox.new(@lists_frame) { listvariable @all_msgs ; height 10 }
             @game_field = TkCanvas.new(@root) { width 1024 ; height 768 }
             @status_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
             @status_label = TkLabel.new(@status_frame) { }
@@ -44,11 +57,33 @@ module Paidgeeks
             @game_field.grid row: 0, column: 1, rowspan: 2, sticky: "nsew"
             @status_frame.grid row: 1, column: 0, columnspan: 2, sticky: "nsew"
 
+            Thread.new { do_update_thread }
+
             Tk.mainloop
           ensure
-            log_file.close()
+            @log_file.close()
           end
         end
+
+        def do_update_thread
+          loop do
+            begin
+              msg = Paidgeeks.read_object(@log_file, 1.0)
+              process_msg(msg) if !msg.nil?
+            rescue StandardError => e
+              $stderr.write("do_update_thread() exiting (#{e.inspect})\n")
+              return
+            end
+          end
+        end
+
+        def process_msg(msg)
+          msg_text = msg.inspect
+          $stdout.write("processing message: #{msg_text}\n")
+          #self.all_msgs << msg_text
+          #self.all_msgs.shift(100) if self.all_msgs.length > 10_000
+        end
+
       end
     end
   end
