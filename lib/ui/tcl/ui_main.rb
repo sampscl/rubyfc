d39ec1da4b1542f3751e2a08b36b1e5fd5f86fad
Clay <pdgeek@gmail.com>
2018-01-15 08:36:53 -0500

    ui looking a little better

--- lib/ui/tcl/ui_main.rb
+++ lib/ui/tcl/ui_main.rb
@@ -25,24 +25,24 @@ module Paidgeeks
           log_file, log_file_name = open_log_file
           return 0 if log_file.nil?
           begin
-            root = TkRoot.new { title "Fleet Commander Playback - #{log_file_name}" }
-            lists_frame = TkFrame.new(root) { borderwidth 3 ; relief "flat" }
-            fleets = TkListbox.new(lists_frame) { height 4 }
-            mobs = TkListbox.new(lists_frame) { height 25 }
-            log = TkListbox.new(lists_frame) { height 10 }
-            game_field = TkCanvas.new(root) { width 1024 ; height 768 }
-            status_frame = TkFrame.new(root) { borderwidth 3 ; relief "flat" }
-            status = TkLabel.new(status_frame) { }
+            @root = TkRoot.new { title "Fleet Commander Playback - #{log_file_name}" }
+            @lists_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
+            @fleets = TkListbox.new(@lists_frame) { height 4 }
+            @mobs = TkListbox.new(@lists_frame) { height 25 }
+            @log = TkListbox.new(@lists_frame) { height 10 }
+            @game_field = TkCanvas.new(@root) { width 1024 ; height 768 }
+            @status_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
+            @status_label = TkLabel.new(@status_frame) { }
 
-            fleets.pack fill: "both", expand: 1
-            mobs.pack fill: "both", expand: 1
-            log.pack fill: "both", expand: 1
+            @fleets.pack fill: "both", expand: 1
+            @mobs.pack fill: "both", expand: 1
+            @log.pack fill: "both", expand: 1
 
-            status.pack fill: "both", expand: 1
+            @status_label.pack fill: "both", expand: 1
 
-            lists_frame.grid row: 0, column: 0, sticky: "nsew"
-            game_field.grid row: 0, column: 1, rowspan: 2, sticky: "nsew"
-            status_frame.grid row: 1, column: 0, columnspan: 2, sticky: "nsew"
+            @lists_frame.grid row: 0, column: 0, sticky: "nsew"
+            @game_field.grid row: 0, column: 1, rowspan: 2, sticky: "nsew"
+            @status_frame.grid row: 1, column: 0, columnspan: 2, sticky: "nsew"
 
             Tk.mainloop
           ensure
