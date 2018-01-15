8c3af3bc8b6d01e73c594d99e46c6fabd6d1897a
Clay <pdgeek@gmail.com>
2018-01-15 14:17:01 -0500

    ui playback somewhat working

--- lib/ui/tcl/ui_main.rb
+++ lib/ui/tcl/ui_main.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'tk'
+require 'ostruct'
 
+require_relative '../../../config/config'
 require_relative '../../game_engine/all'
 require_relative '../../utilities/stream_comms'
 
@@ -10,79 +12,101 @@ module Paidgeeks
     module UI
       class TkMain
 
-        attr_accessor :all_msgs
-        attr_accessor :fleets
-        attr_accessor :mobs
+        attr_accessor :dev_null # File to /dev/null
+        attr_accessor :gs # the gamestate
+        attr_accessor :gsc
 
         def initialize
-          self.all_msgs = []
-          self.fleets = []
-          self.mobs = []
-        end
+          self.dev_null = File.open("/dev/null", "r+")
+          self.gs = Paidgeeks::RubyFC::Engine::GameState.new(self.dev_null)
+          self.gsc = Paidgeeks::RubyFC::Engine::GameStateChanger
+          Paidgeeks::RubyFC::Config.load(gs)
 
-        # Get log file and log file name as [log_file, log_file_name]. Call
-        # log_file.close() when done with it.
-        def open_log_file
-          case ARGV[0]
-          when "-"
-            [$stdin, "stdin"]
-          when nil
-            lfn = Tk.getOpenFile
-            File.exist?(lfn) ? [File.open(lfn), lfn] : [nil, nil]
-          else
-            File.exist?(ARGV[0]) ? [File.open(ARGV[0]), ARGV[0]] : [nil, nil]
-          end
-        end
+        end # initialize
 
         def run
-          @log_file, log_file_name = open_log_file
-          return 0 if @log_file.nil?
-          begin
-            @root = TkRoot.new { title "Fleet Commander Playback: #{log_file_name}" }
-            @lists_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
-            @fleets = TkListbox.new(@lists_frame) { listvariable @fleets ; height 4 }
-            @mobs = TkListbox.new(@lists_frame) { listvariable @mobs ; height 25 }
-            @log = TkListbox.new(@lists_frame) { listvariable @all_msgs ; height 10 }
-            @game_field = TkCanvas.new(@root) { width 1024 ; height 768 }
-            @status_frame = TkFrame.new(@root) { borderwidth 3 ; relief "flat" }
-            @status_label = TkLabel.new(@status_frame) { }
-
-            @fleets.pack fill: "both", expand: 1
-            @mobs.pack fill: "both", expand: 1
-            @log.pack fill: "both", expand: 1
-
-            @status_label.pack fill: "both", expand: 1
-
-            @lists_frame.grid row: 0, column: 0, sticky: "nsew"
-            @game_field.grid row: 0, column: 1, rowspan: 2, sticky: "nsew"
-            @status_frame.grid row: 1, column: 0, columnspan: 2, sticky: "nsew"
-
-            Thread.new { do_update_thread }
-
-            Tk.mainloop
-          ensure
-            @log_file.close()
-          end
-        end
-
-        def do_update_thread
-          loop do
-            begin
-              msg = Paidgeeks.read_object(@log_file, 1.0)
-              process_msg(msg) if !msg.nil?
-            rescue StandardError => e
-              $stderr.write("do_update_thread() exiting (#{e.inspect})\n")
-              return
+          log_file, log_file_name =
+            case ARGV[0]
+            when "-"
+              [$stdin, "stdin"]
+            when nil
+              lfn = Tk.getOpenFile
+              File.exist?(lfn) ? [File.open(lfn), lfn] : [nil, nil]
+            else
+              File.exist?(ARGV[0]) ? [File.open(ARGV[0]), ARGV[0]] : [nil, nil]
             end
-          end
-        end
+
+          return -1 if log_file.nil?
+
+          root = TkRoot.new { title "Fleet Commander Playback: #{log_file_name}" }
+          if log_file_name != "stdin"
+            tick_once_cmd = proc do
+              msg = Paidgeeks.read_object(log_file, 1.0)
+              self.process_msg(msg) if msg
+            end
+
+            tick_many_cmd = proc do
+              $stdout.write("tick_many\n")
+            end
+
+            buttons_frame = TkFrame.new(root) { borderwidth 3 ; relief "flat" }
+            tick_button = TkButton.new(buttons_frame) { text "Tick Once" ; command tick_once_cmd }
+            tick_many_button = TkButton.new(buttons_frame) { text "Tick..." ; command tick_many_cmd }
+            tick_button.pack fill: "both", expand: 1
+            tick_many_button.pack fill: "both", expand: 1
+            game_field = TkCanvas.new(root) { width 1024 ; height 768 }
+            game_field.grid row: 0, column: 1, sticky: "nsew"
+            buttons_frame.grid row: 0, column: 0
+            # end if reading from game log file
+          else # else reading from stdin
+            game_field = TkCanvas.new(root) { width 1024 ; height 768 }
+            game_field.grid row: 0, column: 0, sticky: "nsew"
+
+            Thread.new do
+              loop do
+                begin
+                  msg = Paidgeeks.read_object(log_file, 1.0)
+                  self.process_msg(msg) if msg
+                rescue StandardError => e
+                  $stderr.write("do_update_thread() exiting (#{e.inspect})\n")
+                  return
+                end
+              end
+            end
+
+          end # end else using stdin
+
+          Tk.mainloop
+        end # run
 
         def process_msg(msg)
-          msg_text = msg.inspect
-          $stdout.write("processing message: #{msg_text}\n")
-          #self.all_msgs << msg_text
-          #self.all_msgs.shift(100) if self.all_msgs.length > 10_000
-        end
+          $stdout.write("processing #{msg.inspect}\n")
+          case msg["type"]
+          when "init_mission"
+            # do nothing, init mission doesn't do anything for playback.
+          when "game_config"
+            # do nothing, game config doesn't do anything for playback.
+          when "begin_tick", "end_tick"
+            # do nothing, the tick updates are handled within the tick_msg
+          when /_notify$/
+            # do nothing, the notify messages do not affect game state
+          when "add_fleet"
+            # we do not want a real fleet here
+            new_ff = File.expand_path("../../utilities/null_fleet.rb", __FILE__)
+            msg["ff"] = new_ff
+            self.gsc.send("#{msg["type"]}_msg".to_sym, self.gs, msg)
+          when "scan_report"
+            # do nothing, scan reports don't do anything for playback
+          when "turn_to"
+            # do nothing, turn_to calls mob functions that, from a state
+            # perspective, are reduntant with integration messages
+          when "turn_forever"
+            # do nothing, turn_forever calls mob functions that, from a state
+            # perspective, are reduntant with integration messages
+          else
+            self.gsc.send("#{msg["type"]}_msg".to_sym, self.gs, msg)
+          end
+        end # process_msg
 
       end
     end
