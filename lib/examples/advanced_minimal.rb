601e77f162a507428b764bb7026c8ea2842b6ff2
Clay <pdgeek@gmail.com>
2018-03-15 07:13:41 -0400

    advanced_minimal growing towards a real fleet management system

--- lib/examples/advanced_minimal.rb
+++ lib/examples/advanced_minimal.rb
@@ -12,21 +12,63 @@ require_relative '../utilities/math_utils'
 # mob class used to store mobs
 require_relative '../utilities/mob'
 
-# Convenience function to send a message Hash to RubyFC, the
-# flush is important to make sure that the game receives messages
-# in a timely manner.
-def send_msg(msg)
-  Paidgeeks.write_object($stdout, msg)
-  $stdout.flush
-end
+class Fleet
+  attr_accessor :enemies # {mid: mob, ...}
+  attr_accessor :mobs # {mid: ship_instance, ...}
 
-# Convenience method to write a log entry. Each fleet
-# has it's own log file created for it.
-# Parameters:
-# - msg => The message to write, will be turned into a string
-#   and written to the log file.
-def log(msg)
-  $stderr.puts(msg)
+  def initialize
+    self.enemies = {}
+    self.mobs = {}
+  end
+
+  # Convenience function to send a message Hash to RubyFC, the
+  # flush is important to make sure that the game receives messages
+  # in a timely manner.
+  def Fleet.send_msg(msg)
+    Paidgeeks.write_object($stdout, msg)
+    $stdout.flush
+  end
+
+  # Convenience method to write a log entry. Each fleet
+  # has it's own log file created for it.
+  # Parameters:
+  # - msg => The message to write, will be turned into a string
+  #   and written to the log file.
+  def self.log(msg)
+    $stderr.puts(msg)
+  end
+
+  # Process messages received from the game
+  # Parameters:
+  # - msg => The message; this will be a Hash object with
+  #   String keys.
+  def process(msg)
+    case msg["type"]
+    when "end_tick" # end of a game tick
+      Fleet.send_msg({"type" => "tick_acknowledged", "tick" => msg["tick"]})
+
+    when "munition_intercept_notify"
+      if msg["remaining_target_hitpoints"] <= 0
+        enemies.delete(msg["target_mid"])
+      end
+
+    when "create_mob_notify" # new mob of mine
+      Fleet.log(msg)
+      case msg["template"]
+      when "Paidgeeks::RubyFC::Templates::Gunship"
+        self.mobs[msg["mid"]] = Cruiser.new(msg["mid"])
+      end
+
+    when "scan_report"
+      msg["reports"].each { |report| enemies[report["mid"]] = report }
+
+    when "warn_fleet"
+      Fleet.log(msg)
+    end
+
+    # let all the ships see the message too
+    mobs.each_value { |ship_instance| ship_instance.process(self,msg)}
+  end
 end
 
 class Cruiser
@@ -36,78 +78,58 @@ class Cruiser
     # "azimuth" => degrees,
     # "range" => game_units, gunship inherits default scan range of 1500.0
     # }
-  attr_accessor :enemies # {mid: mob, ...}
   attr_accessor :mob # my gunship
   attr_accessor :waypoints # [{x: x, y: y}, ...]
 
-  def initialize
+  def initialize(mid)
     range_for_30_degree_scan = 143.0 # 30 degrees to radians = area / range; solve for range
-    range_for_3_degree_scan = 1043.0 # 30 degrees to radians = area / range; solve for range
-    self.enemies = {}
-    self.mob = nil
+    range_for_3_degree_scan = 1043.0 # 3 degrees to radians = area / range; solve for range
     self.scan_plan = [ # scans always cover 75 square units on a 3600 x 2400 playing field (config.yml)
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 15.0,"range" => range_for_30_degree_scan }, # 0 - 30
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 45.0,"range" => range_for_30_degree_scan }, # 30 - 60
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 75.0,"range" => range_for_30_degree_scan }, # 60 - 90
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 105.0,"range" => range_for_30_degree_scan }, # 90 - 120
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 135.0,"range" => range_for_30_degree_scan }, # 120 - 150
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 165.0,"range" => range_for_30_degree_scan }, # 150 - 180
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 195.0,"range" => range_for_30_degree_scan }, # 180 - 210
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 225.0,"range" => range_for_30_degree_scan }, # 210 - 240
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 255.0,"range" => range_for_30_degree_scan }, # 240 - 270
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 285.0,"range" => range_for_30_degree_scan }, # 270 - 300
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 315.0,"range" => range_for_30_degree_scan }, # 300 - 330
-      { "type" => "scan", "source_ship" => 0, "azimuth" => 345.0,"range" => range_for_30_degree_scan }, # 330 - 360
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 15.0,"range" => range_for_30_degree_scan }, # 0 - 30
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 45.0,"range" => range_for_30_degree_scan }, # 30 - 60
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 75.0,"range" => range_for_30_degree_scan }, # 60 - 90
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 105.0,"range" => range_for_30_degree_scan }, # 90 - 120
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 135.0,"range" => range_for_30_degree_scan }, # 120 - 150
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 165.0,"range" => range_for_30_degree_scan }, # 150 - 180
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 195.0,"range" => range_for_30_degree_scan }, # 180 - 210
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 225.0,"range" => range_for_30_degree_scan }, # 210 - 240
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 255.0,"range" => range_for_30_degree_scan }, # 240 - 270
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 285.0,"range" => range_for_30_degree_scan }, # 270 - 300
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 315.0,"range" => range_for_30_degree_scan }, # 300 - 330
+      { "type" => "scan", "source_ship" => mid, "azimuth" => 345.0,"range" => range_for_30_degree_scan }, # 330 - 360
     ]
-    (1..120).each { |i| self.scan_plan << { "type" => "scan", "source_ship" => 0, "azimuth" => i * 3.0, "range" => range_for_3_degree_scan } }
+    (1..120).each { |i| self.scan_plan << { "type" => "scan", "source_ship" => mid, "azimuth" => i * 2.999, "range" => range_for_3_degree_scan } }
     self.waypoints = [
       { x: 100, y: 2300},
       { x: 3500, y: 2300},
       { x: 3500, y: 100},
       { x: 100, y: 100},
     ]
+    self.mob = Paidgeeks::RubyFC::Mob.new
+    mob.mid = mid
   end
 
-  # Process messages received from the game
-  # Parameters:
-  # - msg => The message; this will be a Hash object with
-  #   String keys.
-  def process(msg)
-    case msg["type"] # all messages have a "type" field
-    when "begin_tick" # beginning of a game tick
-      send_msg(scan_plan.first)
+  def process(fleet, msg)
+    case msg["type"]
+    when "begin_tick"
+      Fleet.send_msg(scan_plan.first)
       scan_plan.rotate!
+
     when "end_tick" # end of a game tick
-      send_msg({"type" => "tick_acknowledged", "tick" => msg["tick"]})
-      #process_engagements()
-      #log(enemies.pretty_inspect)
-    when "munition_intercept_notify"
-      if msg["remaining_target_hitpoints"] <= 0
-        enemies.delete(msg["target_mid"])
-      end
-    when "create_mob_notify" # new mob of mine, fly in a circle
-      log(msg)
-      if mob.nil?
-        self.mob = Paidgeeks::RubyFC::Mob.from_msg(msg)
-        mid = msg["mid"]
-        scan_plan.each { |sp| sp["source_ship"] = mid } # update scan plan with my mob id
-      end
+      process_engagements(fleet)
+
     when "integrate_mob_notify"
       if self.mob && self.mob.mid = msg["mid"]
         self.mob = Paidgeeks::RubyFC::Mob.from_msg(msg)
-        process_waypoints(msg)
+        process_waypoints(fleet, msg)
       end
-    when "scan_report"
-      msg["reports"].each { |report| enemies[report["mid"]] = report }
-    else # this is a message that we don't handle yet, just log it
-      #log("Got message that I don't handle: #{msg.inspect}")
     end
   end
 
-  def process_engagements
+  def process_engagements(fleet)
   end
 
-  def process_waypoints(integrate_msg)
+  def process_waypoints(fleet, integrate_msg)
     # am I close to my next waypoint?
     wpt = waypoints.first
     rng2 = Paidgeeks::range2(mob.x_pos, mob.y_pos, wpt[:x], wpt[:y])
@@ -120,9 +142,9 @@ class Cruiser
     # get heading I should use to reach next waypoint
     new_hdg_rad = Paidgeeks::normalize_to_circle(Paidgeeks::relative_angle(mob.x_pos, mob.y_pos, wpt[:x], wpt[:y]))
 
-    # do I need to turn to get there?
+    # turn to new heading of I need to
     if !Paidgeeks::is_near(new_hdg_rad, mob.heading)
-      send_msg({
+      Fleet.send_msg({
         "type" => "turn_to",
         "mid" => mob.mid,
         "heading" => Paidgeeks.rad_to_deg(new_hdg_rad),
@@ -137,15 +159,14 @@ end # class Cruiser
 # the command line. Which happens to be *exactly* what RubyFC does
 # when you give it a fleet file name.
 def main
-
   # always write your fleet metadata first
-  send_msg({
+  Fleet.send_msg({
       "type"       => "set_fleet_metadata",
       "author"     => "Clay Sampson",
       "fleet_name" => "Advanced Minimal Example 1.0",
     })
 
-  fleet = Cruiser.new
+  fleet = Fleet.new
 
   # Keep going forever, RubyFC will clean up the fleets automagically
   loop do
